06729945c3c6e6e2a50d6fd6deac5dab2bc0b73e
Bhavan Kumar V <76980923+bhavankumar2911@users.noreply.github.com>
2024-12-14 23:49:25 +0530

    Update buildDockerImage.groovy

--- vars/buildDockerImage.groovy
+++ vars/buildDockerImage.groovy
@@ -22,7 +22,7 @@ def call(Map params) {
     echo "Dockerfile path: ${dockerfilePath}"
 
     // Build the Docker image using the Jenkins docker plugin
-    def customImage = docker.build("${imageName}:${tag}", ".")
+    def customImage = docker.build("${ecrRepoUrl}:${tag}", ".")
 
     // Tag the image for AWS ECR (Optional)
     def ecrRepoUrl = params.get('ecrRepoUrl', '')
